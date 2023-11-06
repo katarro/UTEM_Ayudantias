0248edfaed0f5ddb0c2316932f88591a9f49ce02
felipe <pipe.castro.espinoza@gmail.com>
2023-11-06 00:09:29 -0300

    V1.1

--- fronted/src/components/adminIn/AdminIn.jsx
+++ fronted/src/components/adminIn/AdminIn.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Adminin.css'; // Asegúrate de importar tu archivo CSS
+import './Adminin.css';
 
 export default function AdminIn() {
   const [postulantes, setPostulantes] = useState([]);
   const [filteredPostulantes, setFilteredPostulantes] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
-  const [isLoading, setIsLoading] = useState(true); // Estado para controlar el indicador de carga
+  const [isLoading, setIsLoading] = useState(true);
+  const [sortOrder, setSortOrder] = useState('asc');
+  const [filterAsignatura, setFilterAsignatura] = useState('');
+  const [notaRange, setNotaRange] = useState({ min: 1.0, max: 7.0 });
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -17,8 +20,8 @@ export default function AdminIn() {
           throw new Error('Hubo un problema al obtener los datos');
         }
         const data = await response.json();
-        setPostulantes(data.postulantes); // Asumiendo que la API devuelve un objeto con una propiedad postulantes
-        setFilteredPostulantes(data.postulantes); // Inicializar los postulantes filtrados
+        setPostulantes(data.postulantes);
+        setFilteredPostulantes(data.postulantes);
       } catch (error) {
         console.error('Error al cargar los postulantes:', error);
       } finally {
@@ -29,13 +32,41 @@ export default function AdminIn() {
   }, []);
 
   useEffect(() => {
-    // Filtrar postulantes cuando el searchTerm cambie
-    const results = postulantes.filter(postulante =>
-      postulante.nombre.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      postulante.rut.includes(searchTerm)
+    let results = postulantes;
+
+    // Filtro por nombre o RUT
+    if (searchTerm) {
+      results = results.filter(postulante =>
+        postulante.nombre.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        postulante.rut.includes(searchTerm)
+      );
+    }
+
+    // Filtro por asignatura
+    if (filterAsignatura) {
+      results = results.filter(postulante =>
+        postulante.asignatura === filterAsignatura
+      );
+    }
+
+    // Filtro por rango de notas
+    results = results.filter(postulante =>
+      postulante.nota >= notaRange.min && postulante.nota <= notaRange.max
     );
+
+    // Ordenar por nota
+    results.sort((a, b) => {
+      const notaA = parseFloat(a.nota);
+      const notaB = parseFloat(b.nota);
+      if (sortOrder === 'asc') {
+        return notaA - notaB;
+      } else {
+        return notaB - notaA;
+      }
+    });
+
     setFilteredPostulantes(results);
-  }, [searchTerm, postulantes]);
+  }, [searchTerm, postulantes, sortOrder, filterAsignatura, notaRange]);
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
@@ -45,31 +76,89 @@ export default function AdminIn() {
     navigate(`/adminin/${item.rut}`, { state: { postulante: item } });
   };
 
+  const handleSortOrderChange = (e) => {
+    setSortOrder(e.target.value);
+  };
+
+  const handleAsignaturaChange = (e) => {
+    setFilterAsignatura(e.target.value);
+  };
+
+  const handleNotaMinChange = (e) => {
+    setNotaRange({ ...notaRange, min: e.target.value });
+  };
+
+  const handleNotaMaxChange = (e) => {
+    setNotaRange({ ...notaRange, max: e.target.value });
+  };
+
+  const handleClearFilters = () => {
+    setSearchTerm('');
+    setSortOrder('asc');
+    setFilterAsignatura('');
+    setNotaRange({ min: 1.0, max: 7.0 });
+  };
+
   if (isLoading) {
     return <div className="loader">Cargando...</div>;
   }
 
+  const sortNotes = (ascending = true) => {
+    setFilteredPostulantes(prevPostulantes => {
+      const postulantesSorted = [...prevPostulantes];
+      postulantesSorted.sort((a, b) => {
+        return ascending ? a.nota - b.nota : b.nota - a.nota;
+      });
+      return postulantesSorted;
+    });
+  };
+
+  // Función para filtrar por estado de aprobación
+  const filterByApproval = (approved) => {
+    const results = postulantes.filter(postulante => postulante.estado === (approved ? 'Aprobado' : 'Rechazado'));
+    setFilteredPostulantes(results);
+  };
+
   return (
-    <div className="container mt-5 mb-5">
-      <div className="row mb-4">
-        <div className="col-12">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Buscar por nombre o RUT..."
-            value={searchTerm}
-            onChange={handleSearchChange}
-          />
+    <div className='container'>
+
+      
+      <div className="filters">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Buscar por nombre o RUT..."
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+
+        <div className="filter-buttons mt-5">
+          <button className="filter-btn" onClick={() => filterByApproval(true)}>Aprobados</button>
+          <button className="filter-btn" onClick={() => filterByApproval(false)}>Rechazados</button>
+          <button className="filter-btn" onClick={() => sortNotes(true)}>Ordenar Nota ↗</button>
+          <button className="filter-btn" onClick={() => sortNotes(false)}>Ordenar Nota ↘</button>
+        </div>
+        <div className='clean-filter'>
+          <button className='btn btn-secondary' onClick={handleClearFilters}>Limpiar Filtros</button>
         </div>
       </div>
-      <div className="row">
+
+
+
+      <div className="admin-container">
         {filteredPostulantes.map((item, index) => (
-          <div key={index} className="col-sm-6 col-lg-4 mb-4" onClick={() => handleItemClick(item)}>
-            <div className="card h-100">
-              <div className="card-body">
-                <h5 className="card-title">{item.nombre}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">{item.asignatura}</h6>
-                <p className="card-text">{item.nota}</p>
+          <div key={index} className="row-container" onClick={() => handleItemClick(item)}>
+            <div className="row-content">
+              <div className='nombre'>
+                <h5 className="row-title">{item.nombre}</h5>
+              </div>
+              <div className='asignatura'>
+
+                <h6 className="row-subtitle">{item.asignatura}</h6>
+              </div>
+              <div className='nota'>
+
+                <p className="row-text">{item.nota}</p>
               </div>
             </div>
           </div>
